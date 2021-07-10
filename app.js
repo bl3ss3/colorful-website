80c7f3270870562829220954766ad70cad0ba916
bl3ss3 <spendmoney303@gmail.com>
2021-07-10 06:13:51 -0600

    Final

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
-
+const navLogo = document.querySelector('#navbar__logo');
 //display menu
 const mobileMenu = () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-}
+};
 
 menu.addEventListener('click', mobileMenu);
+
+
+// Show active menu when scrolling
+const hightlightMenu = () => {
+    const elem = document.querySelector('highligt')
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
+    let scrollPos = widow.scrollY
+
+    // adds 'highlight class to my menu items
+    if(window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('hightlight');
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        servicesMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        return 
+    } 
+
+if((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
+}
+
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click'. highlightMenu);
+
+// Close Mobile Menu with clcking on a menu item
+const hideMenu = ()  => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click'. hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
+
+
+
